7d914f231b953fa078a59634141ac4942ab2429d
Gustavo Pereira de Souza <gustavopereira@sistemafieto.com.br>
2019-10-15 15:33:36 -0300

    Adicionando variavel ambiente

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-app.user(cors());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://sesi:sesieventos@sesi-eventos-iztyf.mongodb.net/flashclip?retryWrites=true&w=majority", {
     useNewUrlParser: true,
@@ -16,4 +16,4 @@ mongoose.connect("mongodb+srv://sesi:sesieventos@sesi-eventos-iztyf.mongodb.net/
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
